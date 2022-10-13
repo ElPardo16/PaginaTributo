977785df00be06e1cfae37b88b197f26d781b112
ElPardo16 <andresito105@gmail.com>
2022-10-12 20:55:55 -0500

    arreglos hover menu mobile

--- src/script.js
+++ src/script.js
@@ -1,9 +1,12 @@
 let menu = document.getElementById("menu")
 let boton = document.getElementById("m-b")
+let fondo = document.getElementById("f-menu")
 let click = false;
+let idOld = "m-1"
 
 function clickMenu(){
     animBoton();
+    click ? fondo.classList.add("hideF") : fondo.classList.remove("hideF")
     click ? menu.classList.add("hide") : menu.classList.remove("hide")
     click = !click
 }
@@ -11,3 +14,26 @@ function animBoton(){
     boton.classList.add("anim")
     setTimeout(() => boton.classList.remove("anim"), 400)
 }
+function hoverMenu(id){
+    clickMenu();
+    hover(id)
+}
+function hover(id){
+    document.getElementById(idOld).classList.remove("select")
+    document.getElementById(id).classList.add("select")
+    idOld = id;
+}
+function scrollPos(){
+    for (let index = 1; index < 6; index++) {
+        //const element = array[index];
+        let altura = window.innerHeight * (index - 1) 
+        //console.log(altura)
+        if(window.scrollY == altura){
+            hover(`m-${index}`)
+        }
+        
+    }
+}
+fondo.addEventListener("click", () => clickMenu())
+
+
